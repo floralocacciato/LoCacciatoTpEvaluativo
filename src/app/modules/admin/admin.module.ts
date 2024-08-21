f89c5bc1d77738435779d5caae1dbecfba18982a
floralocacciato <florencialocacciato@gmail.com>
2024-08-21 10:58:51 -0300

    cambios en el inicio-sesion

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -1,14 +1,40 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+//archivo de rutas del modulo
 import { AdminRoutingModule } from './admin-routing.module';
+
+// Componente de VISTA
+import { AdminComponent } from './pages/admin/admin.component';
+
+// Componente LOCAL
+import { TableComponent } from './components/table/table.component';
+
+// Componente de MATERIAL
+import { MatIconModule } from '@angular/material/icon';
+
+// Paqueterías de formularios y formularios reactivos de ANGULAR
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AdminComponent, 
+    TableComponent
+  ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule
+  ], 
+  exports: [
+    AdminComponent,
+    TableComponent,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class AdminModule { }
